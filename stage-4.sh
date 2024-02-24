6e99aed1a75b4dd16004f764201165678fb36567
Lincoln Thurlow <lincoln@isi.edu>
2024-02-23 20:55:27 -0800

    installing etcd cluster and proxy in test environment
    Signed-off-by: Lincoln Thurlow <lincoln@isi.edu>

---
+++ stage-4.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [[ $EUID -ne 0 ]]; then
+        echo "requires root access to run ansible playbook"
+        exit 1
+fi
+
+ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook \
+        -i .rvn/ansible-hosts -i ansible/variables/hosts.ini \
+        -e 'ansible_python_interpreter=/usr/bin/python3' \
+        -e "@ansible/variables/config.yml" \
+        ansible/plays/create_etcd_cluster.yml
