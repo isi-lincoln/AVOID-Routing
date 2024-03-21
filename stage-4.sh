988c05f1e7add2aa39740a48eb425c33fa0bbc3f
Lincoln Thurlow <lincoln@isi.edu>
2024-03-21 12:37:48 -0700

    etcd deployed on nebula
    Signed-off-by: Lincoln Thurlow <lincoln@isi.edu>

--- stage-4.sh
+++ stage-4.sh
@@ -5,7 +5,7 @@ if [[ $EUID -ne 0 ]]; then
         exit 1
 fi
 
-ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook \
+ANSIBLE_HOST_KEY_CHECKING=False ANSIBLE_ROLES_PATH=ansible/roles ansible-playbook \
         -i .rvn/ansible-hosts -i ansible/variables/hosts.ini \
         -e 'ansible_python_interpreter=/usr/bin/python3' \
         -e "@ansible/variables/config.yml" \
